9653f5a72e33058908b53f49b1e02ee81384c0a9
Ricky Landry <104511702+0xXerkan@users.noreply.github.com>
2026-04-01 15:32:40 -0400

    Update facial detection to use Google MediaPipe model instead of Clarifai

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import React, {Component} from 'react';
-import Navigation from './components/navigation/Navigation';
-import Logo from './components/logo/Logo';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-import Rank from './components/Rank/Rank';
-import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-import SignIn from './components/SignIn/SignIn';
-import Register from './components/Register/Register';
-import ParticlesBg from 'particles-bg';
-import './App.css';
-import SERVER_URL from './components/Constants.js';
+import React, { Component } from "react";
+import Navigation from "./components/navigation/Navigation";
+import Logo from "./components/logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
+import SignIn from "./components/SignIn/SignIn";
+import Register from "./components/Register/Register";
+import ParticlesBg from "particles-bg";
+import "./App.css";
+import SERVER_URL from "./components/Constants.js";
+import { FaceDetector, FilesetResolver } from "@mediapipe/tasks-vision";
 
 const initialState = {
-      input: '',
-      imgURL:'',
-      boxes: [],
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-}
-
+  input: "",
+  imgURL: "",
+  boxes: [],
+  route: "signin",
+  isSignedIn: false,
+  user: {
+    id: "",
+    name: "",
+    email: "",
+    entries: 0,
+    joined: "",
+  },
+};
 
 class App extends Component {
   constructor() {
@@ -32,98 +32,136 @@ class App extends Component {
     this.state = initialState;
   }
 
-  loadUser = (data) => {
-    this.setState({user: {
-      id: data.id,
-      name: data.name,
-      email: data.email,
-      entries: data.entries,
-      joined: data.joined
-    }})
-  }
+  faceDetector = null;
 
-  calcFaceLocation = (data) => {
-    return data.outputs[0].data.regions.map(face => {
-      const clarifaiFace = face.region_info.bounding_box;
-      const image=document.getElementById('inputImage');
-      const width = Number(image.width);
-      const height = Number(image.height);
-      return {
-        leftCol: clarifaiFace.left_col * width,
-        topRow: clarifaiFace.top_row * height,
-        rightCol: width - (clarifaiFace.right_col * width),
-        bottomRow: height - (clarifaiFace.bottom_row * height)
-      }
+  getOrCreateFaceDetector = async () => {
+    if (this.faceDetector) return this.faceDetector;
+    const vision = await FilesetResolver.forVisionTasks(
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm",
+    );
+    //define model asset path here. More models can be found here: https://ai.google.dev/edge/mediapipe/solutions/vision/face_detector#models
+    const modelAssetPath =
+      "https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_full_range/float16/1/blaze_face_full_range.tflite";
+
+    this.faceDetector = await FaceDetector.createFromOptions(vision, {
+      baseOptions: {
+        modelAssetPath,
+        delegate: "GPU",
+      },
+      runningMode: "IMAGE",
     });
-  }
+    return this.faceDetector;
+  };
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      },
+    });
+  };
+
+  calcFaceLocation = async (imageUrl) => {
+    const img = new Image();
+    img.crossOrigin = "anonymous";
+    await new Promise((resolve, reject) => {
+      img.onload = resolve;
+      img.onerror = reject;
+      img.src = imageUrl;
+    });
+
+    const faceDetector = await this.getOrCreateFaceDetector();
+    const result = faceDetector.detect(img);
+
+    const renderedWidth = 500;
+    const scaleFactor = renderedWidth / img.naturalWidth;
+    const renderedHeight = img.naturalHeight * scaleFactor;
+
+    return result.detections.map((detection) => {
+      const { originX, originY, width, height } = detection.boundingBox;
+      return {
+        leftCol: originX * scaleFactor,
+        topRow: originY * scaleFactor,
+        rightCol: renderedWidth - (originX + width) * scaleFactor,
+        bottomRow: renderedHeight - (originY + height) * scaleFactor,
+      };
+    });
+  };
 
   displayFaceBox = (boxes) => {
-    this.setState({boxes: boxes});
-  }
+    this.setState({ boxes: boxes });
+  };
 
   onInputChange = (event) => {
-    this.setState({input: event.target.value});
-  }
+    this.setState({ input: event.target.value });
+  };
 
-  onButtonSubmit = () => {
-    this.setState({imgURL: this.state.input});
-    fetch(`${SERVER_URL}/imageAPI`, {
-              method: 'post',
-              headers: {'Content-Type': 'application/json'},
-              body: JSON.stringify({
-                input: this.state.input                
-              })
-            })
-      .then(response => response.json())
-      .then(res => {
-          if (res) {
-            fetch(`${SERVER_URL}/image`, {
-              method: 'put',
-              headers: {'Content-Type': 'application/json'},
-              body: JSON.stringify({
-                id: this.state.user.id                
-              })
-            })
-            .then(res => res.json())
-            .then(count => {
-              this.setState(Object.assign(this.state.user, {entries: count}))
-            })
-          }
-        this.displayFaceBox(this.calcFaceLocation(res))})
-        .catch(error => console.log('error', error));
-  }
+  onButtonSubmit = async () => {
+    this.setState({ imgURL: this.state.input });
+    try {
+      const boxes = await this.calcFaceLocation(this.state.input);
+      this.displayFaceBox(boxes);
+      fetch(`${SERVER_URL}/image`, {
+        method: "put",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          id: this.state.user.id,
+        }),
+      })
+        .then((res) => res.json())
+        .then((count) => {
+          this.setState(Object.assign(this.state.user, { entries: count }));
+        });
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
 
   onRouteChange = (route) => {
-    if (route === 'signin') {
-      this.setState(initialState)
-    } else if (route ==='home') {
-      this.setState({isSignedIn: true})
+    if (route === "signin") {
+      this.setState(initialState);
+    } else if (route === "home") {
+      this.setState({ isSignedIn: true });
     }
-    this.setState({route: route});
-  }
+    this.setState({ route: route });
+  };
 
   render() {
     return (
       <div className="App">
-        <ParticlesBg type="cobweb" bg={true} num={100} color='#EBEBEB' />
-        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
-        { this.state.route === 'home'
-          ?
+        <ParticlesBg type="cobweb" bg={true} num={100} color="#EBEBEB" />
+        <Navigation
+          isSignedIn={this.state.isSignedIn}
+          onRouteChange={this.onRouteChange}
+        />
+        {this.state.route === "home" ? (
           <div>
-              <Logo />
-              <Rank name={this.state.user.name} entries={this.state.user.entries} />
-              <ImageLinkForm 
-                onInputChange={this.onInputChange}
-                onButtonSubmit={this.onButtonSubmit}
-              />
-              <FaceRecognition boxes={this.state.boxes} imgURL={this.state.imgURL} />
+            <Logo />
+            <Rank
+              name={this.state.user.name}
+              entries={this.state.user.entries}
+            />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              boxes={this.state.boxes}
+              imgURL={this.state.imgURL}
+            />
           </div>
-          : (
-              this.state.route === 'signin'
-              ? <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
-            )          
-        }
+        ) : this.state.route === "signin" ? (
+          <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register
+            loadUser={this.loadUser}
+            onRouteChange={this.onRouteChange}
+          />
+        )}
       </div>
     );
   }
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import './ImageLinkForm.css'
+import React from "react";
+import "./ImageLinkForm.css";
 
-const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
-	return (
-			<div>
-				<p className='f3'>
-				{'This Magic Brain will detect faces in your pictures. Give it a try!'}
-				</p>
-				<div className='center'>	
-					<div className='form center pa4 br3 shadow-5'>
-						<input className='f4 pa2 w-70 center' type='text' onChange={onInputChange} />
-						<button 
-							className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
-							onClick={onButtonSubmit} 
-						>Detect</button>
-					</div>
-				</div>
-			</div>
-		);
-}
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+  return (
+    <div>
+      <p className="f3">
+        This Magic Brain will detect faces in your pictures. Give it a try!
+      </p>
+      <p className="f3">
+        Enter the URL of an image that contains faces and click the Detect
+        button.
+      </p>
+      {/* <p className="f3">
+        Tip: You can try these URLs for testing:
+        <br />
+        https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdPRpLNmVVJuPPFpBczltxW7wLa63HEkpD971resYrquyJpDM1OtibEoVo8d1F7pjqNciwMrdQcTfByDOLW1DqDnTypKbbCgRFnHsBnrWwlQ&s=10
+        <br /> <br />
+        https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRx0A8lZiL-5ybiTk6lx3vJPkh-1npUdX31L3RKTnvdkOEDAy9knb4d-8We7q_ib_r0iSKzgiyiMuWxJVvWfP2R4WjezAxr4meAezCSgNSwKw&s=10
+      </p> */}
+      <div className="center">
+        <div className="form center pa4 br3 shadow-5">
+          <input
+            className="f4 pa2 w-70 center"
+            type="text"
+            onChange={onInputChange}
+          />
+          <button
+            className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple"
+            onClick={onButtonSubmit}
+          >
+            Detect
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ImageLinkForm;
